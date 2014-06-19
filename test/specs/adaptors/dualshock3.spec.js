05822900c6af0ab7578e6f44eb1f573411eca8d9
Andrew Stewart <andrew@stwrt.ca>
2014-06-19 15:37:38 -0700

    Remove existing Xbox 360 and DualShock drivers

--- test/specs/adaptors/dualshock3.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var DualShock3 = source("adaptors/dualshock3");
-
-describe("Cylon.Adaptors.Joystick.DualShock3", function() {
-  var adaptor = new DualShock3({ initialize: false });
-
-  it("needs tests");
-});
